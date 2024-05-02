6aa31f088137c8f4e0369f08667c796223e38548
FlezyCodes <srppolinn1@gmail.com>
2024-05-02 18:53:42 +0300

    Inicio

---
+++ src/main/java/github/pvp/Manager.java
@@ -0,0 +1,16 @@
+package github.pvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Manager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
